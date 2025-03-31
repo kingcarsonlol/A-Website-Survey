17d192363d130e67a87f59cf78c77022f9bc047e
Remy <72723803+kingcarsonlol@users.noreply.github.com>
2025-03-31 17:02:43 +0800

    add home page

---
+++ src/lib/state/userStyles.svelte.js
@@ -0,0 +1,13 @@
+export const userStyles = $state({
+	button: [
+		"bg-gray-200",
+		"text-black",
+		"px-2",
+		"py-1 border",
+		"border-gray-400",
+        "rounded-md",
+        "hover:bg-gray-300",
+        "focus:outline",
+        "focus:outline-blue-500"
+	]
+});
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+import { userStyles } from "$lib/state/userStyles.svelte";
+import { goto } from "$app/navigation";
+</script>
+
+<div class="h-[100vh] flex flex-col items-center justify-center">
+    <h1 class="text-8xl mb-10">A Website Survey</h1>
+    <button class="text-3xl {userStyles.button.join(" ")}" on:click={() => goto("/survey")}>
+        START
+    </button>
+</div>
